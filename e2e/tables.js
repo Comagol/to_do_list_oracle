5cb8d840b7e8a50a2b82a2c8e7747e11a8a64e50
comagol <comabellafrancisco@gmail.com>
2026-05-05 19:03:26 -0300

    Add end-to-end test for APEX Playwright application
    - Introduced tables.js to automate the process of creating a page in the APEX application.
    - Implemented navigation and interaction steps for signing in and creating a page with specified parameters.
    - Updated APEX Playwright e2e configuration by removing specific globs for test files to simplify usage instructions.

---
+++ e2e/tables.js
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://g72e895f580b78d-du9gypcf1cci20nt.adb.sa-saopaulo-1.oraclecloudapps.com/ords/r/apex/workspace-sign-in/oracle-apex-sign-in?session=114647267361122');
+  await page.getByRole('textbox', { name: 'Workspace' }).click();
+  await page.getByRole('textbox', { name: 'Workspace' }).fill('*******************');
+  await page.getByText('Workspace Database Username').click();
+  await page.getByRole('textbox', { name: 'Database Username' }).click();
+  await page.getByRole('textbox', { name: 'Database Username' }).fill('*******************');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('*******************');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.getByRole('link', { name: 'Automatic Invoice Handling' }).click();
+  await page.getByRole('button', { name: 'Create Page' }).click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().locator('.a-Icon.icon-region-.page-form').click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('textbox', { name: 'Name' }).fill('form');
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('textbox', { name: 'Name' }).press('Tab');
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('combobox', { name: 'Table / View Name' }).click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('combobox', { name: 'Table / View Name' }).fill('table-pass-by-parameter');
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('switch', { name: 'Use Navigation' }).uncheck();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().locator('.a-Switch-toggle').first().click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('button', { name: 'Next ' }).click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().locator('#P2501_TABLE_NAME_lov_btn').click();
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).fill('table-eter');
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('button', { name: 'Search', exact: true }).click();
+  await page.locator('.ui-dialog.ui-dialog-popuplov > .ui-resizable-handle.ui-resizable-e').click();
+  await page.getByRole('option', { name: 'EMP (table)' }).click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('button', { name: 'Next ' }).click();
+  await page.locator('iframe[title="Create a Page"]').contentFrame().getByRole('button', { name: 'Create Page' }).click();
+});
