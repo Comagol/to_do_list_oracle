7ffadfccbd0349681df13d38ea72a0abc378210f
comagol <comabellafrancisco@gmail.com>
2026-05-13 20:09:12 -0300

    Remove app access control implementation and associated tests

--- e2e/app access control/add_admin_role.spec.js
+++ e2e/app access control/add_admin_role.spec.js
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
-import { createAppAccessControlFromEnv } from './app-access-control.js';
+import { createAppAccessControlFromEnv } from './add_admin_role.js';
 
 test('create app access control', async ({ page }) => {
     test.setTimeout(90_000);
